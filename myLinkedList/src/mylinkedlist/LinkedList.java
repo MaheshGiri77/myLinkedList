081ec4eaba4193d1e16d077b13e5ca46f357afb3
Mahesh Giri <mgiri@stu.ctcd.edu>
2021-12-31 19:48:49 -0600

    Create private node class inside the LinkedList class

---
+++ myLinkedList/src/mylinkedlist/LinkedList.java
@@ -0,0 +1,17 @@
+
+package mylinkedlist;
+
+/**
+ *
+ * @author mahes
+ */
+public class LinkedList {
+    private class Node{
+        private int val;
+        private Node node;
+        public Node(int val){
+            this.val = val;
+        }
+    }
+    
+}
