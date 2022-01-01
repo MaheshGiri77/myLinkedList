bbadedc5aa4dcba55a139565f6ee1337b3c1088c
Mahesh Giri <mgiri@stu.ctcd.edu>
2021-12-31 20:04:54 -0600

    Add the addFirst method

--- myLinkedList/src/mylinkedlist/LinkedList.java
+++ myLinkedList/src/mylinkedlist/LinkedList.java
@@ -8,10 +8,24 @@ package mylinkedlist;
 public class LinkedList {
     private class Node{
         private int val;
-        private Node node;
+        private Node next;
         public Node(int val){
             this.val = val;
         }
+    }
+    
+    private Node head;
+    private Node tail;
+    
+    public void addFirst(int val){
+        Node temp = new Node(val);
+        if(head == null)            //empty linked list
+            head = tail = temp;
+        else{                       //at least one node
+            temp.next = head;       //else temp.next is pointing to add
+            head = temp;            //updata the head 
+        }
+        
     }
     
 }
